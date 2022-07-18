75a9fb98eaa41fad3442090e2d2cc2724866b5d1
tomoya <sunda0707@icloud.com>
2022-07-19 01:55:22 +0900

    Create Dynamic Form

--- js03/main.js
+++ js03/main.js
@@ -1,3 +1,32 @@
+import { v4 as uuid } from "uuid";
+
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("hoge");
+  const app = document.getElementById("js-app");
+  const list = app.querySelector(".js-list");
+
+  const createNewItem = () => {
+    const template = app.querySelector(".js-field_template");
+    const field = template.content.cloneNode(true);
+    const deleteButton = field.querySelector(".js-delete_button");
+    deleteButton.onclick = (event) => {
+      const target = event.target.closest("li");
+      list.removeChild(target);
+    };
+    const li = document.createElement("li");
+    li.setAttribute("data-uuid", uuid());
+    li.appendChild(field);
+    return li;
+  };
+
+  const addButton = app.querySelector(".js-add_button");
+  addButton.onclick = () => {
+    const items = list.querySelectorAll("li");
+    const newItem = createNewItem();
+    if (items.length === 0) {
+      list.appendChild(newItem);
+      return;
+    }
+
+    list.insertBefore(newItem, items[0]);
+  };
 });
