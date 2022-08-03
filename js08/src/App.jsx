745b4476d1899ffb57733ee73076e965870a71a0
tomoya sunda <dexn@digno.localdomain>
2022-08-03 17:34:10 +0900

    Force onChange event to fire
    ref: https://hai3.net/blog/react-trigger-change-outside/

--- js08/src/App.jsx
+++ js08/src/App.jsx
@@ -1,7 +1,26 @@
+import { useRef } from "react";
+
 function App() {
+  const ref = useRef();
+
   return (
-    <div>hoge</div>
-  )
+    <div>
+      <button
+        onClick={() => {
+          ref.current.value = "fuga";
+          ref.current._valueTracker.setValue("");
+          ref.current.dispatchEvent(new Event("change", { bubbles: true }));
+        }}
+      >
+        click
+      </button>
+      <input
+        ref={ref}
+        type="text"
+        onChange={(e) => console.log(e.target.value)}
+      />
+    </div>
+  );
 }
 
-export default App
+export default App;
