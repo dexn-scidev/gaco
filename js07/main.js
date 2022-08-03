0234f723b2cfea34f96c08693876b17b5b660c7f
tomoya sunda <dexn@xeo.local>
2022-08-03 16:03:55 +0900

    Implement sample code

--- js07/main.js
+++ js07/main.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("hoge");
+  const input = document.getElementById("js-input");
+  input.onchange = (e) => {
+    console.log(e.target.value);
+  };
+
+  const btn = document.getElementById("js-button");
+  btn.onclick = () => {
+    input.value = "test";
+    input.dispatchEvent(new Event("change"));
+  };
 });
