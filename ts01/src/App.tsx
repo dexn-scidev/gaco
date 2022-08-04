35582db41ea675a7799ff8de21aa4f43f3f774cd
tomoya sunda <8mo10dance@protonmail.com>
2022-08-04 19:30:46 +0900

    Implement CustomNumberInput

--- ts01/src/App.tsx
+++ ts01/src/App.tsx
@@ -1,5 +1,80 @@
+import { useRef, ChangeEvent, HTMLProps } from "react";
+
+type Props = {
+  min?: number;
+  max?: number;
+  onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
+  inputProps: HTMLProps<HTMLInputElement>;
+};
+
+const CustomNumberInput = ({
+  min,
+  max,
+  onChange,
+  inputProps,
+  ...props
+}: Props) => {
+  const ref = useRef<any>(null);
+
+  return (
+    <span {...props}>
+      <button
+        onClick={() => {
+          if (ref.current == null) return;
+
+          const value = Number(ref.current.value);
+          if (isNaN(value)) return;
+
+          const nextValue = value - 1;
+          if (min !== undefined && nextValue < min) return;
+
+          ref.current.value = String(nextValue);
+          ref.current._valueTracker.setValue("");
+          ref.current.dispatchEvent(new Event("change", { bubbles: true }));
+        }}
+      >
+        -
+      </button>
+      <input
+        ref={ref}
+        type="text"
+        {...inputProps}
+        onChange={onChange || ((e) => {})}
+      />
+      <button
+        onClick={() => {
+          if (ref.current == null) return;
+
+          const value = Number(ref.current.value);
+          if (isNaN(value)) return;
+
+          const nextValue = value + 1;
+          if (max !== undefined && nextValue > max) return;
+
+          ref.current.value = String(nextValue);
+          ref.current._valueTracker.setValue("");
+          ref.current.dispatchEvent(new Event("change", { bubbles: true }));
+        }}
+      >
+        +
+      </button>
+    </span>
+  );
+};
+
 function App() {
-  return <div>hoge</div>;
+  return (
+    <div>
+      <CustomNumberInput
+        min={0}
+        max={999}
+        inputProps={{
+          inputMode: "decimal",
+        }}
+        onChange={(e) => console.log(e.target.value)}
+      />
+    </div>
+  );
 }
 
 export default App;
