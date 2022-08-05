36a16679158d18108fb13c10a136f037289632ef
tomoya sunda <8mo10dance@protonmail.com>
2022-08-05 17:23:53 +0900

    Implement sample code
    wip
    
    wip2

--- js09/src/App.jsx
+++ js09/src/App.jsx
@@ -1,5 +1,55 @@
+import { useReducer, useEffect } from "react";
+
+const Input = ({ id, value, onChange }) => {
+  useEffect(() => {
+    console.log(value);
+  });
+
+  return <input type="text" value={value} onChange={onChange} />;
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "REPLACE":
+      return state.map((item) =>
+        item.id === action.payload.id
+          ? { ...item, value: action.payload.value }
+          : item
+      );
+    default:
+      return state;
+  }
+};
+
+const defaultList = [
+  { id: 1, value: "hoge" },
+  { id: 2, value: "fuga" },
+  { id: 3, value: "piyo" },
+];
+
 function App() {
-  return <div>hoge</div>;
+  const [list, dispatch] = useReducer(reducer, defaultList);
+
+  const handleChange = (id) => (e) => {
+    dispatch({
+      type: "REPLACE",
+      payload: { id, value: e.target.value },
+    });
+  };
+
+  return (
+    <ul>
+      {list.map((item) => (
+        <li key={item.id}>
+          <Input
+            id={item.id}
+            value={item.value}
+            onChange={handleChange(item.id)}
+          />
+        </li>
+      ))}
+    </ul>
+  );
 }
 
 export default App;
