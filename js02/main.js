34fd626a476029f14d4e927e02f098be3d2817dc
tomoya sunda <8mo10dance@protonmail.com>
2022-08-09 14:48:28 +0900

    サンプルコードを実装する

--- js02/main.js
+++ js02/main.js
@@ -1,1 +1,7 @@
-console.log("hoge");
+const ob = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry.isIntersecting);
+  });
+});
+
+ob.observe(document.getElementById("js-app"));
