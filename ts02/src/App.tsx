1625ea6f1ecef430f13de4b6f8dd18fe97c5f697
tomoya sunda <8mo10dance@protonmail.com>
2022-08-15 12:13:47 +0900

    リストのサンプルコードを実装する

--- ts02/src/App.tsx
+++ ts02/src/App.tsx
@@ -1,16 +1,73 @@
-import { useState, ChangeEvent } from "react";
-import CustomNumberInput from "./CustomNumberInput";
+import { useReducer } from "react";
+import { faker } from "@faker-js/faker";
+import ProductList from "./ProductList";
+
+type Item = {
+  id: string;
+  name: string;
+  value: string;
+};
+
+const range = (from: number, to: number): number[] => {
+  const r: number[] = [];
+  for (let i = from; i < to; i++) {
+    r.push(i);
+  }
+  return r;
+};
+
+const defaultItems: Item[] = range(0, 100).map((i) => ({
+  id: String(i),
+  name: faker.lorem.word(),
+  value: "0",
+}));
+
+const ActionType = {
+  REAPLCE: "REPLACE",
+};
+
+const actions = {
+  replace: (id: string, value: string) => ({
+    type: ActionType.REAPLCE as typeof ActionType.REAPLCE,
+    payload: { id, value },
+  }),
+};
+
+type Action = ReturnType<typeof actions.replace>;
+
+const reducer = (items: Item[], action: Action): Item[] => {
+  switch (action.type) {
+    case ActionType.REAPLCE:
+      return items.map((item) => {
+        if (item.id === action.payload.id)
+          return { ...item, value: action.payload.value };
+
+        return item;
+      });
+    default:
+      return items;
+  }
+};
 
 function App() {
-  const [value, setValue] = useState("0");
+  const [items, dispatch] = useReducer(reducer, defaultItems);
+
+  const setValue = (id: string) => (value: string) => {
+    dispatch(actions.replace(id, value));
+  };
 
   return (
-    <CustomNumberInput
-      min={0}
-      max={999}
-      value={value}
-      onChange={(e: ChangeEvent<HTMLInputElement>) => setValue(e.target.value)}
-    />
+    <ul>
+      {items.map((item) => (
+        <li key={item.id}>
+          <ProductList
+            name={item.name}
+            value={item.value}
+            setValue={setValue(item.id)}
+          />
+        </li>
+      ))}
+    </ul>
   );
 }
 
