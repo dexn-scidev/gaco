74074a7f6c2f4a9829b97c7e977e62e7fd602bdc
tomoya sunda <8mo10dance@protonmail.com>
2022-08-25 20:17:24 +0900

    MUIを使う

--- js02/src/layout.jsx
+++ js02/src/layout.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { CssBaseline } from "@mui/material";
 
 const Layout = ({ children }) => {
   return (
@@ -6,8 +7,20 @@ const Layout = ({ children }) => {
       <head>
         <title>sample</title>
         <meta charSet="utf-8" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        />
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
       </head>
-      <body>{children}</body>
+      <body>
+        <CssBaseline />
+        {children}
+      </body>
     </html>
   );
 };
